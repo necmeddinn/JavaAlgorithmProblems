21ac9760844dab64a8f2537857d470335b4aa7a7
necmeddinn <ncnd87@gmail.com>
2025-04-25 22:14:46 +0300

    Finding Largest Number

--- FindingLargestNumber/Main.java
+++ FindingLargestNumber/Main.java
@@ -1,3 +1,8 @@
+//Soru: Kullanıcıdan alınan 5 sayıyı diziye alarak, en büyük sayıyı bulan Java programı yaz.
+//Örnek:
+//Girdi: 3 9 2 15 6
+//Çıktı: 15
+
 public class Main {
     public static void main(String[] args) {
         int[] dizi = {10,21,32,35,76,85,34,23,65,89,35,67};
