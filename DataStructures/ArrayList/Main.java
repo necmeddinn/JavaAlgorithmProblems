3cb83a5b04b9c0d522008f851b43f06e40b884e1
necmeddinn <ncnd87@gmail.com>
2025-05-15 01:09:33 +0300

    Array List

--- DataStructures/ArrayList/Main.java
+++ DataStructures/ArrayList/Main.java
@@ -10,10 +10,15 @@ public class Main {
         names.add("Mary");
         names.add("Richard");
 
+        names.add(0, "Necmeddin");
+
+        names.set(3, "Sıdıka");
+
         System.out.println(names + "\n");
         System.out.println(names.get(3) + "\n");
         for (String name: names) {
             System.out.println(name);
         }
+        System.out.println(names.size());
     }
 }
